03ce58ca66f0189c12619a42a25b2bea7a128523
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-08-02 16:01:21 -0500

    Modify tests to allow for empty product locations
    Closes #16. This removes all validation that required that a product have a location field, as well as changes the tests to verify that a product can have an empty location. (Represented as ""–a null location still throws a validation exception).

--- server/src/main/java/umm3601/product/ProductController.java
+++ server/src/main/java/umm3601/product/ProductController.java
@@ -229,8 +229,7 @@ public class ProductController {
         .check(product -> product.category != null && product.category.length() > 0,
             "Product must have a non-empty category")
         .check(product -> product.store != null && product.store.length() > 0, "Product must have a non-empty store")
-        .check(product -> product.location != null && product.location.length() > 0,
-            "Product must have a non-empty location")
+        .check(product -> product.location != null, "Product location cannot be null")
         // .check(product -> product.notes != null && product.notes.length() > 0, "Product notes cannot be null")
         // .check(product -> product.tags != null && product.tags.size() >= 0, "Product
         // tags cannot be null")
