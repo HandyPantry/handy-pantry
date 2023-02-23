56a588f5f318050fe375402baa8c2923ef7308eb
Nic McPhee <mcphee@morris.umn.edu>
2023-02-23 14:28:08 -0600

    Quote variable references in `mongoseed.sh`
    DeepSource correctly pointed out that we had issues in `mongoseed.sh`, similar to what `shell-harden` would point out.
    
    This should fix those quoting issues.

--- database/mongoseed.sh
+++ database/mongoseed.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 seed_db="${MONGO_DB:-dev}"
-echo Dropping DB $seed_db
+echo Dropping DB "$seed_db"
 mongo "$seed_db" --eval "db.dropDatabase()"
 mongo "$seed_db" --eval "db.getCollection('products').createIndex({category: 1, store: 1})"
 mongo "$seed_db" --eval "db.getCollection('pantry').createIndex({product: 1})"
 mongo "$seed_db" --eval "db.getCollection('shoppinglist').createIndex({\"products.location\": 1})"
 for file in "$(dirname "$BASH_SOURCE")"/seed/*.json; do
   if [[ -f "$file" ]]; then
-    echo Seeding $(basename "$file" ".json") from $file in DB $seed_db
+    echo Seeding "$(basename $file '.json')" from "$file" in DB "$seed_db"
     mongoimport --db="$seed_db" --collection="$(basename "$file" ".json")" --file="$file" --jsonArray
   fi
 done
